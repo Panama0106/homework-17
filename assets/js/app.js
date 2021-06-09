b9d264fd5bfc35559ee526abc836cf2a142dafde
Panama0106 <panama0106@gmail.com>
2021-06-09 16:28:03 +0200

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 let mass = +prompt('Введите ваш вес');
-let height = +prompt('Введите ваш рост');
+let height = +prompt('Введите ваш рост (в см.)');
 
 let index = mass / ((height / 100) ** 2);
 console.log(`Индекс массы тела равен: ${index}`);
@@ -8,14 +8,14 @@ if (index <= 16) {
     console.log('Выраженный дефицит массы тела');
 } else if (index > 16 && index < 18.5) {
     console.log('Недостаточная (дефицит) масса тела');
-} else if (index > 18.5 && index < 25) {
+} else if (index >= 18.5 && index < 25) {
     console.log('Норма')
-} else if (index > 25 && index < 30) {
+} else if (index >= 25 && index < 30) {
     console.log('Избыточная масса тела (предожирение)');
-} else if (index > 30 && index < 35) {
+} else if (index >= 30 && index < 35) {
     console.log('Ожирение 1 степени');
-} else if (index > 35 && index < 40) {
+} else if (index >= 35 && index < 40) {
     console.log('Ожирение 2 степени');
-} else if (index > 40) {
+} else if (index >= 40) {
     console.log('Ожирение 3 степени');
 }
